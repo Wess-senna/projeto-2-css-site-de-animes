de4935693136bf24c49a938a15d0bbebf06c00c6
Wess-senna <wessenna10@gmail.com>
2025-11-03 22:23:34 -0300

    Atualiza layout e padroniza páginas de animes

--- js/script.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.getElementById("menu-toggle");
-const menu = document.querySelector(".menu ul");
-
-menuToggle.addEventListener("click", () => {
-    menu.classList.toggle("ativo");
-});
